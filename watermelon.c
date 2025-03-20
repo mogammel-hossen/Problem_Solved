c80abb3bcafa75a2c43fd957fba648c97f7fc6ce
mogammel-hossen <mogammel2131@cseku.ac.bd>
2025-03-21 01:13:21 +0600

    Watermelon_problem_solved

---
+++ watermelon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i,a;
+    scanf ("%d",&n);
+    if (n>2&&n%2==0)
+    {
+        printf ("yes");  
+    }
+    else
+    {
+        printf ("no");
+    }
+    return 0;
+}
